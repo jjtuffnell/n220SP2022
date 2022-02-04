6c6b4b3b02b3fb0c0868cd6777b6cfe7d43b8c16
jjtuffnell <97687386+jjtuffnell@users.noreply.github.com>
2022-02-03 21:08:09 -0500

    Update app7.js

--- JSfolder/js/app7.js
+++ JSfolder/js/app7.js
@@ -1,16 +1,19 @@
+//create res variable
 let res;
 
 function setup() {
     createCanvas(600,800);
 }
+//let res = polarPoint function
 function draw () {
     res = polarPoint();
-    translate(100,100);
+    translate(100,100); //translate before drawing method
     circle(res.x, res.y,10); 
 }
+//create polarPoint function
 function polarPoint(r) {
- x = r * Math.sin(mouseX);
- y = r * Math.cos(mouseX);
- return createVector(x,y);
+ x = r * Math.sin(mouseX); //set polarPoint x
+ y = r * Math.cos(mouseX); //set polarPoint y
+ return createVector(x,y); //directions say to write this return but I don't knwo what it does
 }
 

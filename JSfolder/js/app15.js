b0748c9a1bfee16b0e024861a798d9c433f6f6d8
jjtuffnell <97687386+jjtuffnell@users.noreply.github.com>
2022-02-24 21:11:30 -0500

    Loops and Arrays

--- JSfolder/js/app15.js
+++ JSfolder/js/app15.js
@@ -1,23 +1,20 @@
 //create div
 let box = document.getElementById("box");
-//loops numbers 1000,2000,3000,4000,5000
-//one line of code inside loop
-for(var n = 1; n <= 25; n++){
-box.innerHTML += n;
-if(n % 3 === 0){
-    box.innerHTML += "beep <br/>";
-    console.log(n);
-    console.log("yes");
-} else if(n % 5 === 0){
-    box.innerHTML += "bop <br/>";
-    console.log(n);
-    console.log("yes");
-} else if(n % 5 === 0 && n % 3 === 0){
-        box.innerHTML += "beepbop <br/>";
-        console.log(n);
-        console.log("YES");
-    } else
-    box.innerHTML += "</br>";
-    console.log(n);
-    console.log("no");
+
+let sound = "";
+
+for(let n = 1; n <= 25; n++){ //loops 25 times
+
+if(n % 3 == 0){ //check if divisible by 3
+    sound += "beep <div/>";
+    box.innerHTML = sound;
+    
+} else if(n % 5 == 0){ //check if divisble by 5
+    sound += "bop <div/>";
+    box.innerHTML = sound;
+    
+} else if(n % 5 != 0 && n % 3 != 0){
+    sound += "beepbop <div/>"; //checks for both
+    box.innerHTML = sound;
 }
+};
--- JSfolder/js/app16.js
+++ JSfolder/js/app16.js
@@ -1,21 +1,17 @@
 let box1 = document.getElementById("box1");
 let box2 = document.getElementById("box2");
-let box3 = document.getElementById("box3");
+
 box1.style.backgroundColor = "#FF0000";
 box2.style.backgroundColor = "#0F00F0";
-box3.style.backgroundColor = "#FF00F0";
-let myArray = [2.1, 2.25, 3.2];
+box3.style.backgroundColor = "#00FFFF";
 
-for(let i = 0; i < myArray.length; i++){
-    if(myArray[0] < myArray[1]){
-        box1.innerHTML += myArray[0];
-        console.log("yeah");
-    }else if(myArray[1] < myArray[2]){
-        box2.innerHTML += myArray[1];
-        console.log("no");
-    }else if(myArray[2] > myArray[1]){
-        box3.innerHTML += myArray[2];
-    }else {
-    console.log("broken");
-    }
-}
+let myArray = [1.1, 2.25, 3.2];
+
+console.log(myArray);
+
+let second = 0;
+let third = 0;
+
+box2.innerHTML = myArray[1] + " seconds";
+box1.innerHTML = myArray[0] + " seconds";
+box3.innerHTML = myArray[2] + " seconds";
